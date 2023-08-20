7e0c92006c52d74a77621ddc30336539ae6e1523
MSarthak <sarthak.makhija@gmail.com>
2023-08-21 00:32:51 +0530

    Sarthak | Adds code level documentation for payload_provider package

--- payload_provider/file_payload_provider.go
+++ payload_provider/file_payload_provider.go
@@ -4,10 +4,13 @@ import (
 	"os"
 )
 
+// FilePayloadProvider reads the payload from a file.
+// Payload returned by FilePayloadProvider is used in load generation.
 type FilePayloadProvider struct {
 	content []byte
 }
 
+// NewFilePayloadProvider returns the FilePayloadProvider from the provided filePath.
 func NewFilePayloadProvider(filePath string) (*FilePayloadProvider, error) {
 	content, err := os.ReadFile(filePath)
 	if err != nil {
@@ -16,6 +19,8 @@ func NewFilePayloadProvider(filePath string) (*FilePayloadProvider, error) {
 	return &FilePayloadProvider{content: content}, nil
 }
 
+// Get returns the file content.
+// The entire content of the file is read and returned as byte slice.
 func (payloadProvider *FilePayloadProvider) Get() []byte {
 	return payloadProvider.content
 }
