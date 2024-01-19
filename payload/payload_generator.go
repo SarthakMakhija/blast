b70df98441e0984756b32782ae5f0da8c7935ac8
Sarthak <sarthak.makhija@gmail.com>
2024-01-19 11:19:18 +0530

    Sarthak | Adds the documentation for ConstantPayloadGenerator

--- payload/payload_generator.go
+++ payload/payload_generator.go
@@ -1,15 +1,18 @@
 package payload
 
+// ConstantPayloadGenerator provides a constant payload to all the workers for sending the payload.
 type ConstantPayloadGenerator struct {
 	payload []byte
 }
 
+// NewConstantPayloadGenerator creates a new instance of ConstantPayloadGenerator.
 func NewConstantPayloadGenerator(payload []byte) ConstantPayloadGenerator {
 	return ConstantPayloadGenerator{
 		payload: payload,
 	}
 }
 
+// Generate generates (/returns) the same payload for each request.
 func (generator ConstantPayloadGenerator) Generate(_ uint) []byte {
 	return generator.payload
 }
